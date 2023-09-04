855bbe26185da59df56aa8c8f47c02efd42ade3a
unknown <joao.barbosa@canopusonline.com>
2023-09-04 18:27:15 -0300

    ajustes 5

--- app.js
+++ app.js
@@ -10,12 +10,10 @@ const conn = require('./db/conn');
 
 conn();
 
-const port = process.env.PORT || 3000;
-
 //Routes
 const routes = require('./routes/router');
 app.use('/api', routes);
 
-app.listen(port, function () {
+app.listen(3000, function () {
     console.log('Servidor online!!');
 });
